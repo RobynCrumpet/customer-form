4a2002d7c9bed4f4281a926463ae8925137cd731
RobynCrumpet <41733184+RobynCrumpet@users.noreply.github.com>
2023-08-23 18:17:22 +0100

    Update index.js
    I added the wrong index.js before

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import RegisterScreen from './components/RegisterScreen'; // Adjust the path if needed
+import ReactDOM from 'react-dom/client'; 
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-country-select/dist/react-bootstrap-country-select.css';
 
-function App() {
-  return (
-    <div className="App">
-      <RegisterScreen />
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
